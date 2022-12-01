da0311d07f5473902fd100e8296b2c90e531057d
andi <andiyael2208@gmail.com>
2022-11-30 19:07:41 -0600

    javascript&css&html

---
+++ javascript/ejercicio1/index.js
@@ -0,0 +1,11 @@
+
+const lista=["andi",24,true,new Date("august 22 1998 "),libro={
+    titulo:"inventado",
+    autor:"inventado",
+    fecha:"02/11/1990",
+    url:"https://holasoyunaurlinventada"
+
+}
+];
+
+console.log( lista);
